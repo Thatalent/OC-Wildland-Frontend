71c86a3bc785cdbdb40f184fadb3a0f77dfb3371
Darilyn <darilyn.peguero@gmail.com>
2025-10-24 14:41:32 -0400

    my frist commit

--- src/lib/apollo.ts
+++ src/lib/apollo.ts
@@ -1,7 +1,8 @@
 import { ApolloClient, InMemoryCache, createHttpLink } from '@apollo/client'
 
 const httpLink = createHttpLink({
-  uri: import.meta.env.VITE_GRAPHQL_ENDPOINT || 'http://localhost:4000/graphql',
+  uri: import.meta.env.VITE_GRAPHQL_ENDPOINT || 'http://localhost:8080/api/graphql',
+  credentials: 'include',
 })
 
 export const apolloClient = new ApolloClient({
--- vite.config.ts
+++ vite.config.ts
@@ -3,10 +3,14 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  base: './',
   plugins: [react()],
   server: {
     port: 3000,
-    open: true
+    open: true,
+    proxy: {
+      "/api": "http://localhost:8080/api/graphql",
+    },
   },
   build: {
     outDir: 'dist',
